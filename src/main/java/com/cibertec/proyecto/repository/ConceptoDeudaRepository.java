9e898bc0e4808a05299812178a9b745e6de61392
JORGE PUMACALLAHUI PICHIRHUA <I202403118@cibertec.edu.pe>
2026-04-26 18:37:33 -0500

    Commit and Push...

--- src/main/java/com/cibertec/proyecto/repository/ConceptoDeudaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cibertec.proyecto.repository;
-
-import com.cibertec.proyecto.entity.ConceptoDeuda;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConceptoDeudaRepository extends JpaRepository<ConceptoDeuda, Long> {
-}
